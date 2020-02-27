047c467f6b775504255b9424ffc149b5e6ec3476
gen0205 <12490153+gen0205@users.noreply.github.com>
2020-02-27 10:20:28 +0900

    add tmux config and zshrc(need to fix)

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/rc ~/.vim/rc
+ln -sf ~/dotfiles/rc ~/.vim/
 ln -sf ~/dotfiles/colors ~/.vim/
 ln -sf ~/dotfiles/.vim/.cheatsheet.md ~/.vim/.cheatsheet.md
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
