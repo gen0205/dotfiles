bab1fcbc1294cc050c9aa87055ce837c36842e88
gen0205 <12490153+gen0205@users.noreply.github.com>
2020-03-30 10:13:03 +0900

    update installer for mac

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+isMac=0
+if [ "$(uname)" = 'Darwin' ]; then
+  isMac=1
+fi
+
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
@@ -8,3 +13,7 @@ ln -sf ~/dotfiles/rc ~/.vim/
 ln -sf ~/dotfiles/colors ~/.vim/
 ln -sf ~/dotfiles/.vim/.cheatsheet.md ~/.vim/.cheatsheet.md
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+if [ $isMac -eq 1 ]; then
+  brew bundle install --file mac/Brewfile
+fi
