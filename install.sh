a200fe67568c7cab74d2d8421a41366d95f1e807
g-aikawa <g-aikawa@20-2008-L-C011.local>
2021-01-14 17:57:03 +0900

    add .inputrc and .asdfrc and add about mac settings

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ mkdir -p ~/.vim
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.asdfrc ~/.asdfrc
+ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
 ln -sf ~/dotfiles/rc ~/.vim/
@@ -20,6 +22,11 @@ ln -sf ~/dotfiles/.vim/.cheatsheet.md ~/.vim/.cheatsheet.md
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 if [ $isMac -eq 1 ]; then
+  # mac環境の場合
+  # 隠しファイルを表示する
+  defaults write com.apple.finder AppleShowAllFiles -boolean true
+  # 共有フォルダで.DS_Storeファイルを作成しない
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores true
   if !( type brew > /dev/null 2>&1 ); then
     echo "install homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
